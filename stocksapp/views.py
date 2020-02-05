a30c739cdc413505bb6db23764cf1cd69774c5fd
Alexander Törnvall <alextorn.business@gmail.com>
2020-02-05 20:48:21 +0100

    adding frontends; bootstrap, css

--- stocksapp/views.py
+++ stocksapp/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
-# Create your views here.
+
+def about(request):
+    return render(request, 'about.html', {})
