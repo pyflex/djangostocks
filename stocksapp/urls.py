c92a420dd6fe0b91716620d5e1d7c2f015235f57
Alexander Törnvall <alextorn.business@gmail.com>
2020-02-07 21:42:34 +0100

    added full backend func

--- stocksapp/urls.py
+++ stocksapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about'), 
+    path('about', views.about, name='about'),
+    path('add_stock', views.add_stock, name='add_stock'),
+    path('delete/<stock_id>', views.delete, name='delete'),
+    path('delete_stock', views.delete_stock, name="delete_stock"),
 ]
--- stocksapp/views.py
+++ stocksapp/views.py
@@ -1,9 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
+from django.contrib import messages
+from .forms import StockForm
+import time
+import requests
+import json
 
 
 def index(request):
-    return render(request, 'index.html', {})
+    
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_token = "pk_4207608c73a8464fbd181abcd5259a73"
+        api_request = requests.get(
+            f"https://cloud.iexapis.com/stable/stock/{ticker}/quote?token={api_token}&format=json")
+
+        # get info about company /stock/{ticker}/company --> {symbol}
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error"
+        return render(request, 'index.html', {"api": api})
+
+    else:
+        return render(request, 'index.html', {"ticker": "Fill out the ticker in the form above."})
+
+    
 
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def add_stock(request):
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Stock has been added successfully.")
+            return redirect('add_stock')
+
+    else:
+        ticker = Stock.objects.all()
+        output = []
+        api_token = "pk_4207608c73a8464fbd181abcd5259a73"
+
+
+        for ticker_item in ticker:
+            # time.sleep(0.001)
+            api_request = requests.get(f"https://cloud.iexapis.com/stable/stock/{str(ticker_item)}/quote?token={api_token}&format=json")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error"
+
+        return render(request, 'add_stock.html', {"ticker": ticker, "output": output})
+
+
+def delete(request, stock_id):
+    try:
+        item = Stock.objects.get(pk=stock_id)
+        item.delete()
+        messages.success(request, "Item has been deleted.")
+        return redirect('delete_stock')
+    except Exception:
+        messages.error(request, "There is no such stock to delete.")
+        return redirect('delete_stock')
+
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, "delete_stock.html", {"ticker": ticker})
+
+# using messages in django: https://docs.djangoproject.com/en/3.0/ref/contrib/messages/
